e4fc2c55006a8e6afdefd71b014665aff96a882a
Alafresh <868@holbertonschool.com>
2020-05-12 15:43:12 -0500

    Bust a move

---
+++ 0x02-unity-scripting/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    public float speed;
+    public Rigidbody rb;
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+            rb.AddForce(0, 0, speed * Time.deltaTime);
+        if (Input.GetKey("s"))
+            rb.AddForce(0, 0, -speed * Time.deltaTime);
+        if (Input.GetKey("a"))
+            rb.AddForce(-speed * Time.deltaTime, 0, 0);
+        if (Input.GetKey("d"))
+            rb.AddForce(speed * Time.deltaTime, 0, 0);
+    }
+}
