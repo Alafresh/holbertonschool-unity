a1816dbd2aaf7f62ffe730eb9d65f414806ca2f4
Alafresh <868@holbertonschool.com>
2020-05-12 18:29:17 -0500

    Game over

--- 0x02-unity-scripting/Assets/Scripts/PlayerController.cs
+++ 0x02-unity-scripting/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public int health = 5;
     private int score = 0;
     [SerializeField]
     public float speed;
     public Rigidbody rb;
+    void Update()
+    {
+        if (health == 0)
+        {
+            Debug.Log("Game Over!");
+            SceneManager.LoadScene("maze");
+        }
+    }
     void FixedUpdate()
     {
         if (Input.GetKey("w"))
@@ -26,6 +34,15 @@ public class PlayerController : MonoBehaviour
             Destroy(other.gameObject);
             score += 1;
             Debug.Log("Score: " + score);
+        }
+        if (other.tag == "Trap")
+        {
+            health -= 1;
+            Debug.Log("Health: " + health);
+        }
+        if (other.tag == "Goal")
+        {
+            Debug.Log("You win!");
         }
     }
 }
