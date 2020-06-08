0da32d9193e4a77da0b9e3a9efd8bbb1e3c8938d
Alafresh <868@holbertonschool.com>
2020-06-08 16:42:00 -0500

    add new porject

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Range(0.01f, 1.0f)]
+    public float smoothFactor = 0.5f;
+    [SerializeField]
+    public GameObject player;
+    public Vector3 offset;
+    public float rotationSpeed;
+    void LateUpdate()
+    {
+        Quaternion camAngle =
+            Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationSpeed, Vector3.up);
+
+        offset = camAngle * offset;
+        transform.position = player.transform.position + offset;
+        Vector3 newPos = player.transform.position + offset;
+        transform.position = Vector3.Slerp(transform.position, newPos, smoothFactor);
+        transform.LookAt(player.transform);
+    }
+}
---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    public Rigidbody rb;
+    public float speed;
+    public float jumpForce;
+    bool isOnGround;
+    public float rotationSpeed;
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveX, 0f, moveZ);
+        transform.Translate(movement * (speed * Time.deltaTime));
+        
+        if (isOnGround && Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            isOnGround = true;
+        }
+    }
+}
